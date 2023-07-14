c8876788b20f0493f460e7c530cd945f08ba2a68
Olga Shiryavceva <olga@newagesports.net>
2023-07-14 19:30:55 +0300

    feat: view controllers

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,18 +10,25 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButtonComponent: UIButton!
+    @IBOutlet weak var stopButtonComponent: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stopButtonComponent.isEnabled = false
     }
 
     @IBAction func recordButton(_ sender: Any) {
         recordingLabel.text = "Recording in Progress"
+        stopButtonComponent.isEnabled = true
+        recordButtonComponent.isEnabled = false
     }
     
     @IBAction func stopButton(_ sender: Any) {
-        recordingLabel.text = "stop recording button was pressed"
+        recordButtonComponent.isEnabled = true
+        stopButtonComponent.isEnabled = false
+        recordingLabel.text = "Tap to Record"
     }
 }
 
